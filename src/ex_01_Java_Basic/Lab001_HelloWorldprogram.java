bd6e3fffbb5352ca2effe1865447bfd9401ecd7b
Manish Rai <manishrai20000@gmail.com>
2025-03-08 15:06:42 +0530

    8th march I am pushing my code

---
+++ src/ex_01_Java_Basic/Lab001_HelloWorldprogram.java
@@ -0,0 +1,9 @@
+package ex_01_Java_Basic;
+
+import java.sql.SQLOutput;
+
+public class Lab001_HelloWorldprogram {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
